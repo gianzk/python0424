25c94dbcda31f54a6d02cab9ae44d0f8d6cbfe15
gianzk <47899960+gianzk@users.noreply.github.com>
2024-04-13 15:19:48 +0000

    update

---
+++ modulo1/practica/practica1.py
@@ -0,0 +1,14 @@
+#                 Ejercicios Nro 1
+# Instrucciones : 
+#  crear un repositorio en github  (de forma publica , revisar video parte inicial de la grabacion)
+#  realizar un archivo por problema 
+#  trabajarlo en local y subir los archivos o trabajarlo por codespace
+# 1. Realizar un programa que pida tus datos personales para ingresar por teclado(inputs)
+#  e imprimirlos. 
+# 2. Ingrese un valor por teclado e imprima el tipo de dato
+# 3. Realice un programa que calcule la suma de los numeros hasta el valor ingresado.
+# Ejemplo : Si ingresa 5 se tendra que calcular 1+2+3+4+5
+# 4.Realizar un programa que permita saber si un usario puede obtener un tarjeta de credito
+# condiciones : ser mayor de 18 años , un ingreso mensual de 1000 soles mensual.
+# si cumple con las 2 condiciones imprimir que tipo de tarjeta puede obtener
+# si su ingreso mensual es de 1000 hasta 3000 soles es tarjeta de clasica , mayor a ello tarjeta dorada.
