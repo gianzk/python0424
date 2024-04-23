4aaa1261a508cd6fc81a83fb690f4e9a53a9830d
gianzk <47899960+gianzk@users.noreply.github.com>
2024-04-23 01:09:46 +0000

    update

---
+++ modulo3/ejercicio1.py
@@ -0,0 +1,6 @@
+try:
+    a=int(input("ingrese un numero"))
+    b=int(input("ingrese segundo numero"))
+    print(a/b)
+except:
+    print("sucedio un error")
---
+++ modulo3/ejercicio2.py
@@ -0,0 +1,2 @@
+# realice una funcion que pida numeros e insertelos en una lista , y luego imprimir la division del numero i-1 y i
+# capturar error
